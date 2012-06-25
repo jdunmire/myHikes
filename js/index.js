b0d4c4636faba471d3cc86994f7954f64e521a33
Jerry Dunmire <jerry@dunmire.org>
2012-06-24 22:23:23 -0700

    Added search to tablesorter

--- js/index.js
+++ js/index.js
@@ -5,6 +5,39 @@
 
 var  urlPosition = false;   // used in tfmap.js
 
+function disablePager() {
+  $('#listOfHikes').trigger('destroy.pager');
+  $('tr.tablesorter-filter').css('display', '');
+  $('input[placeholder="lat"]').parent().hide();
+  $('input[placeholder="lon"]').parent().hide();
+  $('input[placeholder="thumbnail"]').hide();
+}
+
+function enablePager() {
+  $('tr.tablesorter-filter').css('display', 'none');
+  var pagerOptions = {
+    container: $(".pager"),
+    // default output: {page}/{totalPages}
+    output: '{startRow} to {endRow} ({totalRows})',
+    updateArrows: true,
+    page: 0,
+    size: 10,
+    fixedHeight: false,
+    removeRows: false,
+    cssNext: '.next',
+    cssPrev: '.prev',
+    cssFirst: '.first',
+    cssLast: '.last',
+    cssPageDisplay: '.pagedisplay',
+    cssPageSize: '.pagesize',
+    cssDisabled: 'disabled'
+  };
+  $("#listOfHikes").tablesorterPager(pagerOptions);
+  $('input[placeholder="lat"]').parent().hide();
+  $('input[placeholder="lon"]').parent().hide();
+
+}
+
 jQuery(document).ready(function() {
     $("#statTable").hide();
     myMap = initMapLayers("map", "none");
@@ -22,36 +55,28 @@ jQuery(document).ready(function() {
     $('.featuredURL').prop('href', "hike.html?track=" + hikeTrack +
         "&ttype='" + ttype +"'");
 
-    var pagerOptions = {
-      container: $(".pager"),
-      // default output: {page}/{totalPages}
-      output: '{startRow} to {endRow} ({totalRows})',
-      updateArrows: true,
-      page: 0,
-      size: 10,
-      fixedHeight: false,
-      removeRows: false,
-      cssNext: '.next',
-      cssPrev: '.prev',
-      cssFirst: '.first',
-      cssLast: '.last',
-      cssPageDisplay: '.pagedisplay',
-      cssPageSize: '.pagesize',
-      cssDisabled: 'disabled'
-    };
-        
     $(function() {
         $("#listOfHikes").tablesorter({
             widthFixed: true,
-            widgets: ["zebra", "resizable", "saveSort"],
+            widgets: ["zebra", "resizable", "saveSort", "filter"],
             sortList: [[2,1]],  // sort by hike date
             headers: {
-            0: { sorter: 'float' },
-            1: { sorter: 'text' },
-            3: { sorter: 'digit' },
-            }
+              0: { sorter: 'float', filter: false},
+              1: { sorter: 'text' },
+              3: { sorter: 'digit' },
+              4: { filter: false },
+              5: { filter: false },
+            },
+            widgetOptions : {
+              filter_cssFilter : 'tablesorter-filter',
+              filter_childRows : false,
+              filter_startsWith: false,
+              filter_ignoreCase : true,
+              filter_searchDelay : 300,
+              filter_functions: {},
+            },
         });
-        $("#listOfHikes").tablesorterPager(pagerOptions)
+        //$("#listOfHikes").tablesorterPager(pagerOptions)
     });
 
     $.get("data/rowList.html", function(html) { 
@@ -59,6 +84,10 @@ jQuery(document).ready(function() {
         $("#listOfHikes tbody").append(html); 
         // let the plugin know that we made a update 
         $("#listOfHikes").trigger("update"); 
+        enablePager();
+        $('input[placeholder="thumbnail"]').parent().append(
+          '<button class="btn searchBtn" onClick="enablePager();"> <i class="icon-remove"></i></button>'
+          );
     }); 
 
 }); // End of ready()
